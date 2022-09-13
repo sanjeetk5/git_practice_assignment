8a71ffbe11ae8dc7302ff060418bbc93da101712
Ritam Kundu <rtamkunducbnits@gmail.com>
2022-09-13 22:31:27 +0530

    created code for palindrome and prime

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+let str="naman"
+
+
+ let bag="";
+  for(let i=str.length-1; i>=1; i--){
+      bag += str[i];
+          
+      }if(bag==str){
+          console.log("Yes");
+      }else{
+          console.log("No");
+      }
